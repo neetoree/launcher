c4df7a7b7d41ab210a762f2b7e8f0744f3e08d22
Alexander Tumin <iamtakingiteasy@eientei.org>
2016-12-07 21:24:26 +0300

    Initial commit

---
+++ src/main/java/org/neetoree/Main.java
@@ -0,0 +1,7 @@
+package org.neetoree;
+
+/**
+ * Created by Alexander <iamtakingiteasy> Tumin on 2016-12-07.
+ */
+public class Main {
+}
---
+++ src/test/java/org/neetoree/MainTest.java
@@ -0,0 +1,13 @@
+package org.neetoree;
+
+import org.junit.Test;
+
+/**
+ * Created by Alexander <iamtakingiteasy> Tumin on 2016-12-07.
+ */
+public class MainTest {
+    @Test
+    public void testMain() {
+
+    }
+}
