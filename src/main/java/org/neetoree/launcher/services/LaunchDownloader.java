f6a36dee25e7ebb16b756910d2c7d37c57ffdaff
Alexander Tumin <iamtakingiteasy@eientei.org>
2016-12-18 15:00:41 +0300

    war

--- src/main/java/org/neetoree/launcher/services/LaunchDownloader.java
+++ src/main/java/org/neetoree/launcher/services/LaunchDownloader.java
@@ -321,7 +321,12 @@ public class LaunchDownloader implements Runnable {
             download(launcher.getString("url"), tmp, launcher.getInt("size"));
 
             if (configService.getPlatform().equals("windows")) {
-                new ProcessBuilder("cmd", "/c", "ping 127.0.0.1 -n 2 && mv \"" + tmp.getAbsolutePath() + "\" \"" + target.getAbsolutePath() + "\"").start();
+                new ProcessBuilder("cmd", "/c", "ping 127.0.0.1 -n 3 && mv \"" + tmp.getAbsolutePath() + "\" \"" + target.getAbsolutePath() + "\"").start();
+                try {
+                    Thread.currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             } else {
                 if (!tmp.renameTo(target)) {
                     throw new IllegalStateException();
